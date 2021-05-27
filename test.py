8b70f6486835a7efbbaf0f8e983c3f3d8e8ad2a4
lanqiao6 <lanqiao6@a.a>
2021-05-27 08:20:37 +0800

    list_print

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 
 import random
-list1 = [[random.randint(0,1)]*5 for i in range(0,5)]
+list1 = [[random.randint(0,1) for j in range(0,15)] for i in range(0,15)]
 print(list1)
-for y in range(0,5):
-    for x in range(0,5):
+for y in range(0,15):
+    for x in range(0,15):
         print(list1[x][y],end=' ')
     print()
 print('=============')
