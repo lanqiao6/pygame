1229ceddb33765502b77def007fd838d38001732
jh <jh@mail.com>
2021-06-28 17:14:48 +0800

    优化

--- random01.py
+++ random01.py
@@ -7,6 +7,7 @@ user_properties = {}
 properties = {'0-X3-(100金币)', '1-X5-(200金币)'}
 #定义一个字典，用于存储用户信息name，age，glod
 user_info = {}
+log_glod = []
 
 
 def register():
@@ -78,14 +79,16 @@ def use_props():
 
 
 def win_or_loss(multi, s, u):
-    if (s >= 10 and u == 'big') or (s < 10 and u == 'small'):
+    global log_glod
+    s_temp = 'big' if s >= 10 else 'small'
+    if s_temp == u:
         print('您赢了！！！')
         user_info['glod'] += (100 * multi)
-        print('您现在有金币：{}'.format(user_info['glod']))
     else:
         print('您输了！')
         user_info['glod'] -= (100 * multi)
-        print('您现在有金币：{}'.format(user_info['glod']))
+    log_glod.append(user_info['glod'])
+    print('您现在有金币：{}'.format(user_info['glod']))
     if (user_info['glod'] <= 0):
         print('您的金币已经用完，感谢您的使用')
         return 0
@@ -93,7 +96,7 @@ def win_or_loss(multi, s, u):
         return 1
 
 
-def buy_props():
+def buy_props(game_round):
     #如果金币是400的倍数，提示是否购买道具
     if user_info['glod'] % 400 == 0:
         shop = input('您现在有金币：{},是否购买道具 yes or no: '.format(user_info['glod']))
@@ -101,28 +104,27 @@ def buy_props():
             #选择要购买的道具，存储用户道具，扣除金币
             good_num = input('请选择要购买第几个道具{}：'.format(properties))
             if good_num == '0':
-                user_properties[str(len(user_properties))] = 'X3'
+                user_properties[str(game_round)] = 'X3'
                 user_info['glod'] -= 100
                 print('购买成功！消耗金币100')
-                print('您现有金币：{}'.format(user_info['glod']))
             elif good_num == '1':
-                user_properties[str(len(user_properties))] = 'X5'
+                user_properties[str(game_round)] = 'X5'
                 user_info['glod'] -= 200
                 print('购买成功！消耗金币200')
-                print('您现有金币：{}'.format(user_info['glod']))
             else:
                 print('没有该道具，您失去了这次机会')
-        else:
-            #打印剩余的金币
-            print('您现有金币：{}'.format(user_info['glod']))
+    print('您现有金币：{}'.format(user_info['glod']))
 
 
 if __name__ == '__main__':
     register()
+    game_round = 0
     while True:
+        game_round += 1
         s, u = start_guess()
         multi = use_props()
         if (win_or_loss(multi, s, u) == 0):
             break
-        buy_props()
-    print('欢迎下次来玩，再见！')
+        buy_props(game_round)
+    log_glod.sort(reverse=True)
+    print('欢迎下次来玩，再见！最高金币：{}'.format(log_glod[0]))
